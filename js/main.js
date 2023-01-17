eddd19f6659e872d34f98b2c57a83fc7f8b851dd
rodrigo martinez duvivier <rodrigo.martinez224@gmail.com>
2023-01-17 13:25:51 -0300

    V13

--- js/main.js
+++ js/main.js
@@ -1,201 +1,237 @@
 
-alert("mira nuestro catalogo de ropa disponibles y elegi, pero primero ingresa tu mail para recibir nuestras ofertas");
+const btnCart = document.querySelector('.container-cart-icon')
+const containerCartProducts = document.querySelector('.container-cart-products')
+
+btnCart.addEventListener('click', () => {
+    containerCartProducts.classList.toggle('hidden-cart')
+})
 
 
-let codigo = "ZXGFI";
+const cartInfo = document.querySelector('cart-product');
+const rowProduct = document.querySelector('.row-product');
 
-function promocion() {
-    let ingresar= false;
-    for (let i = 4; i >= 0; i--){
-        let userMail = prompt("ingresa el codigo de descuento");
-        if (userMail == codigo){
-            alert("gracias por ingresar tu codigo ahora vas a tener un descuento del 5%");
-            ingresar = true;
-            break
-        }else{
-            alert("por favor ingresa un codigo  valido te quedan " + i + "chances");
-        }
-    }
-    return ingresar;
-}
- console.log(promocion());
+// lista de todos los contenedores de productos
+const productslist = document.querySelector('.container-items');
 
- 
+//variable de arreglos de productos
+let allProducts = [];
+
+const valorTotal = document.querySelector('.total-pagar');
+
+const countProducts = document.querySelector('#contador-productos');
+
+const cartEmpty = document.querySelector('.cart-empty');
+const cartTotal = document.querySelector('.cart-total');
 
 
 
+// buscador 
+document.addEventListener('keyup', e =>{
+   if(e.target.matches('#buscador')){
+    document.querySelectorAll(".item").forEach(Articulos => {
+     Articulos.textContent.toLocaleLowerCase().includes(e.target.value)
+     ? Articulos.classList.remove('filtro')
+     : Articulos.classList.add('filtro');  
+    })
 
-let resultado= 0; 
-let cantidad = 0; 
-let total = 0; 
+   } 
+    
+})
 
 
-// listado de objetos (productos)
 
-function Ropa(nombre, precio, stock){
-  this.nombre = nombre;
-  this.precio = parseFloat(precio);
-  this.stock = parseInt(stock);
 
- 
-  this.agregarImpuesto = (impuesto) => {
-    this.precio = this.precio + (this.precio * impuesto ) /100;
+productslist.addEventListener('click', e => {
+	if (e.target.classList.contains('btn-add-cart')) {
+		const product = e.target.parentElement;
+
+		const infoProduct = {
+			quantity: 1,
+			title: product.querySelector('h2').textContent,
+			price: product.querySelector('p').textContent,
+		};
+
+		const exits = allProducts.some(
+			product => product.title === infoProduct.title
+		);
+
+		if (exits) {
+			const products = allProducts.map(product => {
+				if (product.title === infoProduct.title) {
+					product.quantity++;
+					return product;
+				} else {
+					return product;
+				}
+			});
+			allProducts = [...products];
+		} else {
+			allProducts = [...allProducts, infoProduct];
+		}
+
+		showHTML();
+	}
+});
+rowProduct.addEventListener('click', e => {
+	if (e.target.classList.contains('icon-close')) {
+		const product = e.target.parentElement;
+		const title = product.querySelector('p').textContent;
+
+		allProducts = allProducts.filter(
+			product => product.title !== title
+		);
+
+		console.log(allProducts);
+
+		showHTML();
+	}
+});
+
+
+
+
+
+// funcion para mostar HTML
+
+const showHTML = () =>{
+
+    if (!allProducts.length) {
+		cartEmpty.classList.remove('hidden');
+		rowProduct.classList.add('hidden');
+		cartTotal.classList.add('hidden');
+	} else {
+		cartEmpty.classList.add('hidden');
+		rowProduct.classList.remove('hidden');
+		cartTotal.classList.remove('hidden');
+	}
+
+	// Limpiar HTML
+	rowProduct.innerHTML = '';
+
+	let total = 0;
+	let totalOfProducts = 0;
+
+    allProducts.forEach(product =>{
+        const containerProduct = document.createElement('div');
+        containerProduct.classList.add('cart-product');
+
+        containerProduct.innerHTML = `  <div class="info-cart-product">
+        <span class="cantidad-producto-carrito">${product.quantity}</span>
+        <p class="titulo-producto-carrito">${product.title}</p>
+        <span class="precio-producto-carrito">${product.price}</span>
+    </div>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="icon-close"
+    >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M6 18L18 6M6 6l12 12"
+        />
+    </svg>
+
+    `;
+
+    rowProduct.append(containerProduct);
+
+    total =
+			total + parseInt(product.quantity * product.price.slice(1));
+		totalOfProducts = totalOfProducts + product.quantity;
+
+    })
+
+    valorTotal.innerText = `$${total}`;
+	countProducts.innerText = totalOfProducts;
+
 };
 
-/*
-this.StockDisponible=(cantidad)=>{
-  console.log(this.stock)
-  if(cantidad<=this.stock){
-    this.stock - cantidad;
-  }else{
-    console.log("SIN stock");
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// parte del registro 
+
+const formRegister = document.getElementById("formularioRegistro"),
+
+nombre = document.querySelector("#inputNombre"),
+apellido = document.querySelector("#inputApellido"),
+email = document.querySelector("#inputEmail");
+
+
+let usuarios;
+
+// si hay datos en LS usuarios = LS si no hay usuarios =[]
+if(localStorage.getItem("usuarios")) {
+	usuarios = JSON.parse(localStorage.getItem("usuarios"));
+} else{
+	usuarios =[];
+}	
+
+// constructor de usuario 
+class Usuario {
+	constructor(nombre,apellido,email) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.email = email;
+	}
 }
-*/
 
-}
-
-
-/* remeralisanegraL.agregarImpuesto(100);
-console.log(remeralisanegraL.precio);
-
-*/
-
-
-const remeralisanegraL = new Ropa("remera lisa negra L",500, 20);
-const remeralisablancaM = new Ropa("remera lisa blanca M",500, 30);
-const zapatillasnegrasnike = new Ropa("zapatilla negra nike",1000, 10);
-const zapatillasblancasnike = new Ropa("zapatillas blancas nike",1200, 5);
-/*
-remeralisanegraL.StockDisponible(88);
-console.log(remeralisanegraL.stock);*/
-
-remeralisanegraL.agregarImpuesto(5);
-console.log(remeralisanegraL.precio);
-
-
-
-
-const listado= [remeralisanegraL,remeralisablancaM,zapatillasnegrasnike,zapatillasblancasnike];
-
-
-alert("disponemos de esta lista de productos actualmente escriba el numero para agregarlo al carrito  \n 1) remera lisa negra L, \n 2) remera blanca lisa M, \n 3) zapatillas negras nike, \n 4) zapatillas blancas nike ");
-
-let entrada = parseInt(prompt("selecione un producto para el carrito escriba, 5 para finalizar compra"));
-
-const CarritoFinal =[];
-
-function Carrito(){
-  while(entrada != 5){
-
-    switch(entrada){
-      case 1:
-        cantidad= parseInt(prompt("cuantos productos quiere comprar?"));
-        alert("se agrego al carrito " + cantidad + " remeras lisa negra L");
-        if(resultado <= cantidad){
-          total += (cantidad * remeralisanegraL.precio);
-          alert("el valor de su carrito es de $ " + total);  
-        }
-        break;
-
-
-        case 2:
-          cantidad= parseInt(prompt("cuantos productos quiere comprar?"));
-          alert("se agrego al carrito " + cantidad + " remeras lisa blanca M");
-          if(resultado <= cantidad){
-            total += (cantidad * remeralisablancaM.precio);
-            alert("el valor de su carrito es de $ " + total);  
-          }
-          break;
-
-          case 3:
-            cantidad= parseInt(prompt("cuantos productos quiere comprar?"));
-            alert("se agrego al carrito " + cantidad + " zapatillas negras nike");
-            if(resultado <= cantidad){
-              total += (cantidad * zapatillasnegrasnike.precio);
-              alert("el valor de su carrito es de $ " + total);  
-            }
-            break;
-
-
-            case 4:
-            cantidad= parseInt(prompt("cuantos productos quiere comprar?"));
-            alert("se agrego al carrito " + cantidad + " zapatillas blancas nike");
-            if(resultado <= cantidad){
-              total += (cantidad * zapatillasblancasnike.precio);
-              alert("el valor actual  de su carrito es de $ " + total);  
-            }
-            break;
-
-
-    default:
-      alert("el producto no esta disponible actualmente");
-
-      break;
-    }
-
-    CarrgarProducto(CarritoFinal, entrada);
-    entrada = parseInt(prompt("selecione un producto para el carrito escriba, 5 para finalizar compra"));
-   
-  }
+// guardar usuario
+function guardarUsuario(usuario){
+	return usuarios.push(usuario);
 }
 
 
-
-// carga los producto al array "carritofinal"
-function CarrgarProducto(array, entrada) {
-  array.push(entrada);
+// guardar en LS 
+function guardarEnLS(arr) {
+	return localStorage.setItem('usuarios',JSON.stringify(arr));
 }
+// evento
 
-// console.log(CarritoFinal);
+formRegister.addEventListener('submit', (e)=>{
 
+e.preventDefault();
 
-// total de la compra 
+const newUser = new Usuario(nombre.value, apellido.value, email.value);
 
-function sumar(){
-  let TotalCarrito = total;
-  alert("el total de la compra es de $" + TotalCarrito);
-}
+console.log(newUser);
 
+guardarUsuario(newUser);
+guardarEnLS(usuarios);
+formRegister.reset(); 
 
-// filtrar por nombre
+})
 
-function BuscarPorNombre(arr, filtro){
-  const encontrado= arr.filter((listado)=>{
-    return listado.nombre.includes(filtro);
-  })
-  return encontrado;
-}
-const ServEncontrado = BuscarPorNombre(listado,"zapatillas");
+// alert del registro 
 
-console.log(ServEncontrado);
+document.getElementById('registrar').addEventListener('click',function(){
 
+	Swal.fire({
+		icon: "success",
+		titleText:"GRACIAS POR REGISTRARTE",
+		text:"Ahora vas a poder recibir las ultimas ofertas de nuestros productos",
+		iconColor: "purple"
 
+	});
 
-/*
-function FiltrarPorNombre(arr,filtro){
-  const encontrado= arr.filter((ropa)=>{
-    return ropa.nombre.includes(filtro);
-  });
-}
-let buscar=prompt("ingrese la tipo de prenda que busca ejemplo zapatilla o remeras");
-const filtrado = FiltrarPorNombre(ropa,buscar);
-
-*/
-
-
-
-Carrito();
-sumar();
-
-
-alert("gracias por elegirnos vuelva prontos ");
-
-
-
-
-
-
+});
 
 
 
